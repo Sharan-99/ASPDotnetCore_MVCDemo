135121d3d44118013dba6e2b1666bbb924fb21fa
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-04-26 11:37:49 +0530

    first commit

---
+++ ASPDotnetCore_MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPDotnetCore_MVCDemo.Models;
+
+namespace ASPDotnetCore_MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello from MVC core");
+        }
+
+        public IActionResult GetEmployee()
+        {
+            var emp = new Employee {Id=101,Name="Sharan" };
+            return Json(emp);
+        }
+
+        public ActionResult SayHello()
+        {
+            return View();
+        }
+    }
+}
